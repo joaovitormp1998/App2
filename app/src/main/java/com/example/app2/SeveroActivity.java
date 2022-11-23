fda8d1b9d9c34e6acc0d5780d519a332666f17fc
Joao Vitor <joaovitormonsores@hotmail.com>
2022-11-23 03:14:07 -0300

    tonalidade do mostarda + ajustes de layout da segunda tela

--- app/src/main/java/com/example/app2/SeveroActivity.java
+++ app/src/main/java/com/example/app2/SeveroActivity.java
@@ -14,7 +14,7 @@ public class SeveroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.harry_potter);
+        setContentView(R.layout.severo_snape);
         Button btn = findViewById(R.id.button);
         btn.setOnClickListener(new OnClickListener() {
             @Override
